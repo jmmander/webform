c2546f5417fdb8c98b694bea48bc7007b8fc5118
jmmander <maaaander@gmail.com>
2020-01-20 16:26:00 -0500

    Update form.js

--- form.js
+++ form.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-  //sets min date to today
+  //sets valid dates in selector 
   Date.prototype.yyyymmdd = function() {
     var mm = (this.getMonth() + 1).toString(); // getMonth() is zero-based
     var dd = this.getDate().toString();
@@ -24,8 +24,9 @@ $(document).ready(function() {
   $("#implementationManager").change(function() {
     if (this.checked) {
       $("#IMconditional").hide();
-      var imEmail = $("input:email[name='requestersEmail']").val();
-      $("input:email[name='imEmail']").val(imEmail);
+      $('#imEmail').val( $('#requestersEmail').val())
+      //var imEmail = //$("name='requestersEmail']").val();
+      //$("input:email[name='imEmail']").val(imEmail);
     } else {
       $("#IMconditional").show();
     }
@@ -43,18 +44,84 @@ $(document).ready(function() {
   });
 
   //hides and sets SSH, automation and hadoop if admin training selected
-  $("input[name='AdminTraining']").change(function() {
+  $("input[name='adminTraining']").change(function() {
     if (this.id == "yes") {
       $(".adminconditional").hide();
-      $("input:radio[name='Automation/API']").prop("checked", true);
-      $("input:radio[name='SSH']").prop("checked", true);
-      $("input:radio[name='Hadoop']").prop("checked", true);
+      
+      $('input[name="API"]')[0].checked = true;
+      $('input[name="SSH"]')[0].checked = true;
+      $('input[name="Hadoop"]')[0].checked = true;
     } else {
       $(".adminconditional").show();
     }
   });
 
   //hides kubernetes and sets response to false
-  $("input:radio[name='Kubernetes']").prop("checked", false);
   $("#kubernetes").hide();
+  $('input[name="Kubernetes"]')[1].checked = true;
+  
+ function validateEmail(email) {
+  var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(email);
+}
+
+  var $form = $("form#requestForm"),
+    url =
+      "https://script.google.com/macros/s/AKfycbwo0JNckKfRpkSUOVGFuWTBH2LjWy4_OMtZXrVWi4VT4ZUcyio/exec";
+
+  $("#submit").on("click", function(e) {
+     //sets dateTime to submitted datetime for timestamp
+    var today = new Date();
+    var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
+    var time = today.getHours() + ":" + today.getMinutes() + ":" +          today.getSeconds();
+    var dateTime = date+' '+time;
+    $('input[name="dateTime"]').val(dateTime)
+    
+    //server side data validation
+    //creates variables
+   	var reqEmail =  $('input[name="requestersEmail"]').val()
+    var imCheck = $('input[name="implementationManager"]').val()
+    var imEmail = $('input[name="imEmail"]').val()
+    var trainerEmail = $('input[name="trainerEmail"]').val()
+    var customerEmail =$('input[name="customerEmail"]').val()
+    var customerOrgName = $('input[name="customerOrgName"]').val()
+    var platform = $("input[name='platform']:checked").val();
+    var partnerEmail = $('input[name="partnerEmail"]').val();
+    var office = $('input[name="office"]').val(); 
+    var trainingLocation = $('input[name="trainingLocation"]').val(); 
+    var adminTraining = $("input[name='adminTraining']:checked").val();
+    var noStudios = $('input[name="noStudios"]').val();
+    var usersPerStudio = $('input[name="usersPerStudio"]').val();
+    var startDate = $('input[name="startDate"]').val(); 
+    var endDate = $('input[name="endDate"]').val(); 
+    var API = $("input[name='API']:checked").val();
+    var SSH = $("input[name='SSH']:checked").val();
+    var Hadoop = $("input[name='Hadoop']:checked").val();
+    var Kubernetes = $("input[name='Kubernetes']:checked").val();
+    var sendCopy = $('input[name="sendCopy"]').val()
+    
+    //email validation
+    var emails = [reqEmail, imEmail, trainerEmail, customerEmail, partnerEmail]
+    //for (email of emails) 
+      //{if (!$(this).prop('required')){
+        //if (!$(this).val().length === 0){
+          //validateEmail(email)}}
+       //else if ($(this).prop('required')){
+        //if (!$(this).val().length === 0){
+          //validateEmail(email)
+        //}}
+          
+      
+    
+    
+    
+    
+    e.preventDefault();
+    var jqxhr = $.ajax({
+      url: url,
+      method: "GET",
+      dataType: "json",
+      data: $form.serializeObject()
+    }).success(alert("your form has been submitted"));
+  });
 });
